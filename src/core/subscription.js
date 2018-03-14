dffe08120fd1722b2d3a443e8cd585aae690ae6f
autioch <autioch@users.noreply.github.com>
2018-03-14 09:14:03 +0100

    Added todo.

--- src/core/subscription.js
+++ src/core/subscription.js
@@ -2,6 +2,10 @@ export default function subscription(store) {
   let notifyTimeout;
   const listeners = [];
 
+  /* TODO getState should be called only once in notify loop.
+   * This way even if a listener changes state during notify, all components
+   * still recieve current state and in the next notify loop all components will recieve the next state.
+   */
   function notifyListener(listener) {
     listener(store.getState(), store);
   }
